a295f92d2650e93a88097f140290b8dd64050a61
jimogg <jnogana@gmail.com>
2021-07-05 12:50:33 -0400

    models & serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from . import models
@@ -7,3 +8,20 @@ class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = models.User
         fields = ('id', 'email', 'username', 'password', 'avatar')
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    author_name = serializers.StringRelatedField(
+        source='author',
+        read_only=True,
+    )
+
+    author_id = serializers.PrimaryKeyRelatedField(
+        queryset=models.User.objects.all(),
+    )
+
+    class Meta(UserCreateSerializer.Meta):
+        model = models.Recipe
+        fields = '__all__'
+        # fields = ('id', 'title', 'subtitle', 'intro',
+        #           'steps', 'summary', 'likes', 'comments')
