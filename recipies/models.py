fd06b04d8475f8a54b09f801fa88229c38be8b68
jimogg <jnogana@gmail.com>
2021-07-05 15:29:23 -0400

    comment out JSONfield in  Recipe model

--- recipies/models.py
+++ recipies/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    about = models.CharField(max_length=500, blank=True)
 
     def __str__(self):
         return self.name
@@ -16,8 +17,8 @@ class Recipe(models.Model):
     intro = models.CharField(max_length=600)
     steps = models.CharField(max_length=600)
     summary = models.CharField(max_length=600)
-    likes = models.JSONField()
-    comments = models.JSONField()
+    # likes = models.JSONField(blank=True)
+    # comments = models.JSONField(blank=True)
 
     def __string__(self):
         return self.title
