bf0ffb75ed5d2db95a949f7dbe3017867bb9b698
jimogg <jnogana@gmail.com>
2021-07-08 15:27:06 -0400

    add ssl require=True

--- recipe_me/settings.py
+++ recipe_me/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'recipe_me.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
